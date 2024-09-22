dbc9ed24981fd675674d0cb1c4b2c4e61a0d1c78
Antim Prisacaru <antim.prisacaru@selfpay.ro>
2024-09-22 16:28:10 +0300

    feat(interpolation): add pluralization and select using @messageformat/core

--- packages/core/jest.config.ts
+++ packages/core/jest.config.ts
@@ -1,7 +1,7 @@
 /* eslint-disable */
 export default {
   displayName: 'ng-intl',
-  preset: '../jest.preset.js',
+  preset: '../../jest.preset.js',
   setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
   coverageDirectory: '../coverage/ng-intl',
   transform: {
